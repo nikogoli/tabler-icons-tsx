456712e5350f4e3825b8bf0ef6df898660167298
nikogoli <k_m_naoto@yahoo.co.jp>
2023-03-15 21:29:08 +0900

    Update add_pragma.ts

--- add_pragma.ts
+++ add_pragma.ts
@@ -1,13 +1,16 @@
-import { walk } from "https://deno.land/std@0.155.0/fs/mod.ts"
+import { PathLike } from "https://pax.deno.dev/nikogoli/deno_pathlib@0.0.3"
+import { green } from "https://deno.land/std@0.177.0/fmt/colors.ts"
 
-const file_iteratior = walk("./original_tsx",
-  { maxDepth: 1, match: [/\.tsx$/] }
-)
+const stash_dir = new PathLike("original_tsx")
+const data_dir = new PathLike("tsx")
 
-for await (const fl of file_iteratior){
-  const tx = await Deno.readTextFile(fl.path)
-  await Deno.writeTextFile(
-    `./tsx/${fl.name}`,
-    '/** @jsx h */\nimport { h } from "https://esm.sh/preact@10.10.6"\n\n'+ tx
-  )
-}
+await data_dir.iterdirMap(async p => {
+  if (p.is_file()){
+    const tx = await p.read_text()
+    if (tx.startsWith("/**") == false){
+      await p.move(new PathLike(stash_dir, p.name))
+      await p.write_text('/** @jsx h */\nimport { h } from "https://esm.sh/preact@10.10.6"\n\n'+ tx)
+      console.log("add pragma:", green(p.name))
+    }
+  }
+})
